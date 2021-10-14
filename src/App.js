8486530f50a9eb485590156793abb86a00314dd9
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-14 16:44:55 -0700

    convert the app component from a function to a class based component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,47 @@ import React from "react"
 import Todolist from "./components/Todolist"
 import Todoitem from "./components/Todoitem"
 
+
 /*         
         {Todolist.map(item => < Todoitem key={item.id} id={item.id} task={item.task} requiresConcentration={item.requiresConcentration} 
         howOften={item.howOften} affirmation={item.affirmation} completed={item.completed} handleChange={this.handleChange}/>)}
         */        
 
-function App() {
+class App extends React.Component {
+constructor() {
+  super()
+  this.state = {
+    todos: Todolist
+  }
+  this.handleChange = this.handleChange.bind(this)
+}
 
+handleChange(id) {
+  this.setState(prevState => {
+    const updatedTodos = prevState.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    return {
+      todos: updatedTodos
+    }
+  })
+}
+
+render() {
   return (
     <div className="App">
         <h1>To Do's</h1>
 
   {Todolist.map(item => <Todoitem key={item.id} id={item.id} task={item.task} 
-    requiresConcentration={item.requiresConcentration} howOften={item.howOften} affirmation={item.affirmation}/>)}
+    requiresConcentration={item.requiresConcentration} howOften={item.howOften} affirmation={item.affirmation}
+    handleChange={this.handleChange}/>)}
         
     </div>
   );
+}
 }
 
 export default App;
