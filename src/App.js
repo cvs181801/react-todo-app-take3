d165b10206883865da23490c48d0cd776ae65886
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-16 13:34:26 -0700

    change the array.map() method in render() in App component from mapping over Todoslist componenet to mapping over the current state

--- src/App.js
+++ src/App.js
@@ -12,48 +12,25 @@ constructor() {
   
 }
 
-// handleChange(id) {
-//   this.setState(prevState => {
-//     const updatedTodos = prevState.todos.map(todo => {
-//       if (todo.id === id) {
-//           return {
-//             ...todo,
-//             completed: !todo.completed
-//           }    
-//         } else {
-//           return todo;
-//         }
-//       })
-//     console.log("prevState.todos: ", prevState.todos);  
-//     console.log("updated todos: ", updatedTodos);
-//     return {
-//       todos: updatedTodos
-//     }
-//   })
-// }
-
-componentDidUpdate(prevState) {
-  if(prevState.todos !== this.state.todos) {
-      this.setState(prevState => {
-        const updatedTodos = prevState.todos.map(todo => {
-          if (todo.id === id) {
-              return {
-                ...todo,
-                completed: !todo.completed
-              }    
-            } else {
-              return todo;
-            }
-          })
-        console.log("prevState.todos: ", prevState.todos);  
-        console.log("updated todos: ", updatedTodos);
-        return {
-          todos: updatedTodos
+handleChange(id) {
+  this.setState(prevState => {
+    const updatedTodos = prevState.todos.map(todo => {
+      if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }    
+        } else {
+          return todo;
         }
       })
+    console.log("prevState.todos: ", prevState.todos);  
+    console.log("updated todos: ", updatedTodos);
+    return {
+      todos: updatedTodos
     }
-}  //I'm not sure how to call this method correctly using the prevProps, prevState, and snapshot parameters
-
+  })
+}
 
 
 render() {
@@ -61,7 +38,7 @@ render() {
       <div className="App">
           <h1>To Do's</h1>    
 
-        {Todolist.map(item => <Todoitem key={item.id} id={item.id} task={item.task} 
+        {this.state.todos.map(item => <Todoitem key={item.id} id={item.id} task={item.task} 
           requiresConcentration={item.requiresConcentration} howOften={item.howOften} affirmation={item.affirmation}
           completed={item.completed} handleChange={this.handleChange} />) }
     </div>
